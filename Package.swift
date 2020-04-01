e905270acf8d86a6668c333120defd6c603a3fa1
Kaunteya <>
2020-04-01 17:07:36 +0530

    Update

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["NSToast"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/kaunteya/NSMaterialButton", .branch("master"))
+        .package(url: "https://github.com/kaunteya/NSMaterialButton", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
