ab1477da8c5f055e09bb43bd10e9d6ff8f7cc318
kaunteya <k.suryawanshi@gmail.com>
2020-04-13 16:50:24 +0530

    Fixed closed button image

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "NSToast",
     platforms: [
-        .macOS("10.15.0"),
+        .macOS("10.14"),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
